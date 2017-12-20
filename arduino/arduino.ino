1a112c26f8282ecade8638d0d08eb3999fdbd50b
Radek <radek@madenet.pl>
2017-12-20 22:52:51 +0100

    Add on/off soil moisture sensor, optimize pump code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,23 +3,24 @@
 #include <DHT_U.h>
 
 #define IN_DHT 2
-#define IN_BUTTON_DOWN 5
-#define IN_BUTTON_UP 6
-#define IN_WATER_LEVEL_0 7
-#define IN_WATER_LEVEL_25 8
-#define IN_WATER_LEVEL_50 9
-#define IN_WATER_LEVEL_75 10
-#define IN_WATER_LEVEL_100 11
+#define IN_BUTTON_DOWN 4
+#define IN_BUTTON_UP 5
+#define IN_WATER_LEVEL_0 6
+#define IN_WATER_LEVEL_25 7
+#define IN_WATER_LEVEL_50 8
+#define IN_WATER_LEVEL_75 9
+#define IN_WATER_LEVEL_100 10
 #define IN_SOIL_MOISTURE A0
 
+#define OUT_SOIL_MOISTURE 11
 #define OUT_PUMP 12
 #define OUT_BUZZER 13
 
 #define DHTTYPE DHT22
 #define MAX_LOGS 36 // int
-#define TIME_BETWEEN_READINGS 5000
+#define TIME_BETWEEN_READINGS 10000
 #define DISPLAY_RETURN_TIME 5000
-#define WATERING_DURATION 2000
+#define WATERING_DURATION 5000
 
 typedef enum soilMoistureStatus
 {
@@ -49,6 +50,7 @@ void setup()
   pinMode(IN_WATER_LEVEL_75, INPUT_PULLUP);
   pinMode(IN_WATER_LEVEL_100, INPUT_PULLUP);
 
+  pinMode(OUT_SOIL_MOISTURE, OUTPUT);
   pinMode(OUT_PUMP, OUTPUT);
   pinMode(OUT_BUZZER, OUTPUT);
 
@@ -67,23 +69,24 @@ void loop()
   static uint8_t display_delay=0;
   static int readings_no;
   uint8_t button_pushed=0;
-  uint8_t is_dry=0;
 
   if(timer_readings==0 || (millis()-timer_readings)>=TIME_BETWEEN_READINGS)
   {
+    soilMoistureOn();
     readings r={ readings_no++, getHumidity(), getTemperature(), getSoilMoisture(), getWaterLevel() };
     current_log_index=addLog(logs, r);
     timer_readings=millis();
 
     if(getSoilMoistureStatus()==dry)
     {
-      is_dry=1;
+      pump(WATERING_DURATION, true);
     }
+    soilMoistureOff();
 
     //  alarmOn() || alarmOff()
   }
 
-  is_dry=pump(is_dry, WATERING_DURATION);
+  pump(WATERING_DURATION, false);
 
   if(display_delay==0)
   {
