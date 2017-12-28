ee797dc2e4ce21c70d2433b25e4c3cc11efb2a1a
Radek <radek@madenet.pl>
2017-12-28 20:26:21 +0100

    Change button name

--- arduino/button.ino
+++ arduino/button.ino
@@ -3,20 +3,20 @@ uint8_t readButton(int &index)
   static uint8_t button_still_pressed=0;
   uint8_t button_pushed=0;
 
-  if(digitalRead(IN_BUTTON_UP)==LOW)
+  if(digitalRead(IN_BUTTON_MINUS)==LOW)
   {
     if(button_still_pressed==0)
     {
-      pushButtonUp(index);
+      pushButtonMinus(index);
     }
     button_still_pressed=1;
     button_pushed=1;
   }
-  else if(digitalRead(IN_BUTTON_DOWN)==LOW)
+  else if(digitalRead(IN_BUTTON_PLUS)==LOW)
   {
     if(button_still_pressed==0)
     {
-      pushButtonDown(index);
+      pushButtonPlus(index);
     }
     button_still_pressed=1;
     button_pushed=1;
@@ -27,13 +27,13 @@ uint8_t readButton(int &index)
   }
   return button_pushed;
 }
-void pushButtonUp(int &index)
+void pushButtonMinus(int &index)
 {
   index--;
   if(index<0) index=MAX_LOGS-1;
 }
 
-void pushButtonDown(int &index)
+void pushButtonPlus(int &index)
 {
   index++;
   if(index>=MAX_LOGS) index=0;
