010d21508317d65c6b9713e557d3b7b8535b4029
Radek <radek@madenet.pl>
2017-12-28 21:42:07 +0100

    Display only if content was changed

--- arduino/display.ino
+++ arduino/display.ino
@@ -1,27 +1,37 @@
 void display(readings object)
 {
-  lcd.backlight();
+  static int old_item_no=0;
+
+  if(old_item_no==object.item_no)
+  {
+    return;
+  }
+
+  old_item_no=object.item_no;
+
+  lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(object.item_no);
   lcd.print(".");
-  lcd.setCursor(3,0);
+  lcd.setCursor(4,0);
   lcd.print("H:");
   lcd.print(object.humidity);
-  lcd.print("%  T:");
+  lcd.print("% T:");
   lcd.print(object.temperature, 1);
-  lcd.setCursor(2,1);
+  lcd.setCursor(3,1);
   lcd.print("SM:");
   lcd.print(object.soil_moisture);
-  lcd.print("% WL:");
+  lcd.print("% W:");
   lcd.print(object.water_level);
+
   Serial.print(object.item_no);
-  Serial.print(". ");
+  Serial.print(". H:");
   Serial.print(object.humidity);
-  Serial.print("% ");
+  Serial.print("% T:");
   Serial.print(object.temperature, 1);
-  Serial.print("*C ");
+  Serial.print("*C SM:");
   Serial.print(object.soil_moisture);
-  Serial.print("% ");
+  Serial.print("% W:");
   Serial.print(object.water_level);
-  Serial.println("");
+  Serial.println();
 }
